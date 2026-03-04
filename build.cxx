6dcd19a6e31e08c185ce89a53cdef0e55c7bf2d1
Mike <mallisiscool@gmail.com>
2026-03-04 14:36:23 -0500

    ok extrapolate this ig

--- build.cxx
+++ build.cxx
@@ -1,8 +1,16 @@
 #include <cstdlib>
+#include <format>
 #include <iostream>
 
 int main() {
-	auto result = system("clang++ build.cxx -o builder");
+	auto compiler = "clang++";
+	auto output = "builder";
+	auto sources = "build.cxx";
+	auto standard = "c++2c";
+
+	const auto cmd = std::format("{} -std={} -o {} {}", compiler, standard, output, sources);
+	std::cout << "Invoking build command: '" << cmd << "'" << std::endl;
+	const auto result = system(cmd.c_str());
 	if (result != 0) {
 		std::cout << "Build failed: " << result << std::endl;
 	} else {
