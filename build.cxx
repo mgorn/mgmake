c874a781edaea492f7ac22d1cf70956d9ac8dead
Mike <mallisiscool@gmail.com>
2026-03-07 20:55:16 -0500

    the next step ig

--- build.cxx
+++ build.cxx
@@ -1,14 +1,41 @@
 #include <cstdlib>
 #include <format>
 #include <iostream>
+#include <string>
+#include <vector>
+
+struct Toolchain {
+	std::string mCompiler = "clang++";
+};
+
+struct Sources {
+	std::vector<std::string> mSources{};
+
+	operator std::string() const {
+		return str();
+	}
+	[[nodiscard]] std::string str() const {
+		std::string result;
+		for (const auto& source : mSources) {
+			result += source + " ";
+		}
+		return result;
+	}
+};
+
+struct Target {
+	std::string mName;
+	Sources mSources{};
+};
 
 int main() {
-	auto compiler = "clang++";
-	auto output = "builder";
-	auto sources = "build.cxx";
+	Toolchain toolchain;
+	Target target{ "builder", {
+		{ "build.cxx" }
+	} };
 	auto standard = "c++2c";
 
-	const auto cmd = std::format("{} -std={} -o {} {}", compiler, standard, output, sources);
+	const auto cmd = std::format("{} -std={} -o {} {}", toolchain.mCompiler, standard, target.mName, target.mSources.str());
 	std::cout << "Invoking build command: '" << cmd << "'" << std::endl;
 	const auto result = system(cmd.c_str());
 	if (result != 0) {
